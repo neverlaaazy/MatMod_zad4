93fd3febdd11b517e38e516cc8ec0c152e6a7cae
neverlaaazy <cheserega174@gmail.com>
2025-05-21 16:49:57 +0500

    idk dont play animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 const box = document.querySelector(".conFall");
 const boxBtn = document.getElementById("boxButton");
 const circle = document.querySelector(".circle");
+const textTask = document.getElementById("textTask");
+
+const animation = circle.animate(
+    [{ transform: translateY(0) }, { transform: translateY(280) }],
+    {duration: 2000});
+
+// boxBtn.addEventListener("click", function(){
+//     circle.classList.add("circleMove");
+// })
 
 boxBtn.addEventListener("click", function(){
-    circle.classList.add("circleMove");
+    animation.play()
 })
